7e582b307111ed5cc3e855e08f2075c4e2233fb2
self-websitecreation <payal979913@gmail.com>
2026-09-21 16:33:44 +0530

    Accept form input from User

--- script.js
+++ script.js
@@ -9,3 +9,24 @@ let motivationMsg=document.getElementById("motivation-btn-message");
 motivational.addEventListener("click",function(){
 motivationMsg.textContent="Consistency beats Talent .";
 });
+ 
+let userName=document.getElementById("name");
+let userEmail=document.getElementById("email");
+let userMsg= document.getElementById("message");
+let formMsg=document.getElementById("form_msg");
+let form=document.getElementById("contactForm");
+form.addEventListener("submit",function(event){
+    event.preventDefault();
+  let name=userName.value; 
+  let mail=userEmail.value;
+    formMsg.innerHTML= "Thank you "+name+"!"+"for Connecting with me !<br>We will reply to you at "+mail+"<br>Message sent"; 
+console.log(name);
+console.log(mail);
+let msg=userMsg.value;
+console.log(msg);
+let submitBtn=document.getElementById("submit_btn");
+userName.value="";
+userEmail.value="";
+userMsg.value="";
+submitBtn.disabled= true;
+});
